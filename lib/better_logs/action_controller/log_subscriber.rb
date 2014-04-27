ec044faf05ed3f2acb73bd73471ddfb437bd35ac
Kunal Parikh <kunal@techthumb.in>
2014-04-27 12:23:18 +1000

    matching lib & spec directory heirarchy so that Guard can do it's magic

--- lib/better_logs/action_controller/log_subscriber.rb
+++ lib/better_logs/action_controller/log_subscriber.rb
@@ -1,4 +1,10 @@
 class ::BetterLogs::ActionController::LogSubscriber < ActiveSupport::LogSubscriber
+  attr_reader :formatter
+
+  def initialize(formatter)
+    @formatter = (formatter || ::BetterLogs::Formatters::KeyValueFormatter.new)
+  end
+
   def process_action(event)
     logger.info formatter.format(::BetterLogs::ActionController::Payload.new(event.payload))
   end
@@ -6,9 +12,5 @@ class ::BetterLogs::ActionController::LogSubscriber < ActiveSupport::LogSubscrib
   private
   def logger
     Rails.logger
-  end
-
-  def formatter
-    @formatter ||= ::BetterLogs::Formatters::KeyValueFormatter.new
   end
 end
