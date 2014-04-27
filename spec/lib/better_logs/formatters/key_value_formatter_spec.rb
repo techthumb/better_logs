1b95337a04a87163fe5f993b33f7065c1c21c8f8
Kunal Parikh <kunal@techthumb.in>
2014-04-27 13:57:44 +1000

    Minor formatting around KeyValueFormatter spec

--- spec/lib/better_logs/formatters/key_value_formatter_spec.rb
+++ spec/lib/better_logs/formatters/key_value_formatter_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
-describe BetterLogs::Formatters::KeyValueFormatter do
-  let(:payload) { BetterLogs::ActionController::Payload.new({key_one: 'value_one', key_two: 'value_two'}) }
 
-  subject { BetterLogs::Formatters::KeyValueFormatter.new.format(payload) }
-  it { should == 'key_one=value_one key_two=value_two' }
+describe BetterLogs::Formatters::KeyValueFormatter do
+  describe '#format' do
+
+    let(:payload) do
+      BetterLogs::ActionController::Payload.new(key_one: 'value_one', key_two: 'value_two')
+    end
+
+    subject { BetterLogs::Formatters::KeyValueFormatter.new.format(payload) }
+
+    it { should == 'key_one=value_one key_two=value_two' }
+  end
 end
